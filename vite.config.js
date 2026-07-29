fda659ee64ff6d40399a92437ccfd9bdf7d6557c
Friday-z2Z <462032310@qq.com>
2026-07-29 17:00:57 +0800

    修改配置

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/dianayyx2/',  // GitHub Pages 子路径
   plugins: [vue()],
   server: {
     proxy: {
